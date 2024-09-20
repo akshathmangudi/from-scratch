53642d1c82d1008593cdff243845e3e75d7de407
Akshath <akshathmangudi@gmail.com>
2024-09-20 10:36:54 +0530

    archiving last changes

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # from-scratch
+## This project is complete and is currently archived. 
+
 As the name suggests, everything from scratch.
 
 An extension of my other side project, picograd. But here I will be a little more extensive.
@@ -6,7 +8,6 @@ An extension of my other side project, picograd. But here I will be a little mor
 MLP visualization is under assets/
 
 These examples in the notebook can be played around with according to your choice. Right now, my plans are to build: 
-1. RNN and LSTM 
-2. More optimizers 
-3. Multi-Head Attention 
-4. Tokenizers
+1. RNN and LSTM [COMPLETE]
+2. More optimizers [COMPLETE]
+3. Multi-Head Attention [COMPLETE]
--- src/conv.py
+++ src/conv.py
@@ -1,4 +1,4 @@
-import numpy 
+import numpy
 from numpy.lib.stride_tricks import as_strided
 
 """
@@ -19,47 +19,50 @@ Few things to understand:
     The Conv2D and Conv3D simply extend it to further dimensions. Will work on a generalized ConvND as a later exercise
 """
 
+
 class Conv1D:
-    def __init__(self, kernel: int, stride: int, padding: int) -> None: 
-        self.kernel = kernel
+    def __init__(self, kernel: numpy.ndarray, stride: int, padding: int) -> None:
         self.stride = stride
         self.padding = padding
-    
-    def forward(self, in_array: numpy.ndarray) -> numpy.ndarray: 
-        
+        self.kernel = kernel
+
+    def forward(self, in_array: numpy.ndarray) -> numpy.ndarray:
+
         # The forward function defines the convolution operation
-        padded_input = numpy.pad(in_array, pad_width=self.padding, mode="constant", constant_values=0)
+        padded_input = numpy.pad(
+            in_array, pad_width=self.padding, mode="constant", constant_values=0)
 
         out_len = ((len(padded_input) - len(self.kernel)) // self.stride) + 1
         out_array = numpy.ones(out_len)
 
-        for i in range(out_len): 
+        for i in range(out_len):
             idx = i + self.stride
             end = idx + len(self.kernel)
-            out_array[i] = numpy.sum(numpy.multiply(padded_input[idx:end], self.kernel))
+            out_array[i] = numpy.sum(numpy.multiply(
+                padded_input[idx:end], self.kernel))
 
         return out_array
 
 
-
-class Conv2D: 
-    def __init__(self, kernel: numpy.ndarray, stride: int, padding: int) -> None: 
+class Conv2D:
+    def __init__(self, kernel: numpy.ndarray, stride: int, padding: int) -> None:
         self.kernel = kernel
         self.stride = stride
         self.padding = padding
 
-    def forward(self, in_matrix: numpy.ndarray) -> numpy.ndarray: 
+    def forward(self, in_matrix: numpy.ndarray) -> numpy.ndarray:
         i_w, i_h = in_matrix.shape
         k_w, k_h = self.kernel.shape
 
-        padded_input = numpy.pad(in_matrix, ((self.padding, self.padding), (self.padding, self.padding)), mode="constant", constant_values=0)
+        padded_input = numpy.pad(in_matrix, ((self.padding, self.padding), (
+            self.padding, self.padding)), mode="constant", constant_values=0)
 
         o_h = ((i_h + 2*self.padding - k_h) // self.stride) + 1
         o_w = ((i_w + 2*self.padding - k_w) // self.stride) + 1
 
         out_matrix = numpy.ones((o_w, o_h))
 
-        for i in range(o_w): 
+        for i in range(o_w):
             for j in range(o_h):
                 o_i = i * self.stride
                 o_j = j * self.stride
@@ -68,13 +71,14 @@ class Conv2D:
                 j_end = o_j + len(self.kernel)
 
                 sub_matrix = padded_input[o_i:i_end, o_j:j_end]
-                out_matrix[i, j] = numpy.sum(numpy.multiply(sub_matrix, self.kernel))
+                out_matrix[i, j] = numpy.sum(
+                    numpy.multiply(sub_matrix, self.kernel))
 
         return out_matrix
 
 
-class Conv3D: 
-    def __init__(self, kernel: numpy.ndarray, stride: int, padding: int) -> None: 
+class Conv3D:
+    def __init__(self, kernel: numpy.ndarray, stride: int, padding: int) -> None:
         self.kernel = kernel
         self.stride = stride
         self.padding = padding
@@ -90,17 +94,19 @@ class Conv3D:
         i_d, i_w, i_h = in_tensor.shape
         k_d, k_w, k_h = self.kernel.shape
 
-
-        padded_input = numpy.pad(in_tensor, ((self.padding, self.padding), (self.padding, self.padding), (self.padding, self.padding)), mode="constant", constant_values=0)
+        padded_input = numpy.pad(in_tensor, ((self.padding, self.padding), (self.padding,
+                                 self.padding), (self.padding, self.padding)), mode="constant", constant_values=0)
 
         o_d = ((i_d + 2*self.padding - k_d) // self.stride) + 1
         o_h = ((i_h + 2*self.padding - k_h) // self.stride) + 1
         o_w = ((i_w + 2*self.padding - k_w) // self.stride) + 1
 
         shape = (o_d, o_h, o_w, k_d, k_h, k_w)
-        strides = (self.stride * padded_input.strides[0], self.stride * padded_input.strides[1], self.stride * padded_input.strides[2], padded_input[0], padded_input[1], padded_input[2])
+        strides = (self.stride * padded_input.strides[0], self.stride * padded_input.strides[1],
+                   self.stride * padded_input.strides[2], padded_input[0], padded_input[1], padded_input[2])
 
         windows = as_strided(padded_input, shape=shape, strides=strides)
 
-        out_tensor = numpy.tensordot(windows, self.kernel, axes=((3, 4, 5), (0, 1, 2)))
+        out_tensor = numpy.tensordot(
+            windows, self.kernel, axes=((3, 4, 5), (0, 1, 2)))
         return out_tensor
